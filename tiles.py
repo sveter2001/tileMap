5eadb759177cc819035978bb3980165bc00996b6
Ivan Kostiuchkov (a.k.a. i-401) <sveter2001@mail.ru>
2022-11-21 21:00:10 +0300

    Add files via upload
    tiles [WIP]

---
+++ tiles.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Level():
+    pass
+
+class chunk():
+
+    def __init__(self):
+        self.tiles = []
+
+    pass
+
+class tile():
+
+    def __init__(self, length, width, image):
+        self.length = length
+        self.width = width
+        self.image = image
+        self.TTL = 10
+
+
+
+    pass
